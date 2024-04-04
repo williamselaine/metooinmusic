6835332295c6f044818a9ea5d86b7aa6a626491b
williamselaine <elainelwil@gmail.com>
2024-04-04 11:05:03 +0200

    initial imp

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,41 +1,31 @@
-import GoogleMapReact from 'google-map-react';
-import MapTheme from '../constants/MapTheme.json';
-
-const defaultProps = {
-  center: {
-    lat: 50.9,
-    lng: 10.4
-  },
-  zoom: 7,
-  style: MapTheme
-};
+import { Chart } from 'react-google-charts';
 
 function Map() {
+
+  const data = [
+    ["Latitude", "Longitude"],
+    [51.387420, 7.003440],
+  ];
+
+  var options = {
+    region: 'DE',
+    colorAxis: {
+      minValue: 'c7c0e4',
+      maxValue: 'c7c0e4',
+    },
+    dataMode: 'markers',
+    datalessRegionColor: 'ffffff',
+    legend: 'none',
+    magnifyingGlass: false,
+  };
+
   return (
     <>
-        <GoogleMapReact
-          bootstrapURLKeys={{ key: import.meta.env.VITE_APP_MAPS_API_KEY }}
-          defaultCenter={defaultProps.center}
-          defaultZoom={defaultProps.zoom}
-          options={{
-            styles: defaultProps.style,
-            minZoom: 5.5,
-            scaleControl: false,
-            zoomControl: false,
-            fullscreenControlOptions: {
-              position: 9,
-            },
-            restriction: {
-              latLngBounds: {
-                west: 0,
-                east: 22,
-                south: 44,
-                north: 56,
-              }
-            },
-            strictBounds: true,
-          }}
-        ></GoogleMapReact>
+      <Chart
+        chartType="GeoChart"
+        data={data}
+        options={options}
+      />
     </>
   );
 }
