51c1bb5950ee05affa7f88639705993316bef91f
ewi <elaine.williams@maluma.world>
2025-01-01 20:59:41 +0100

    put tooltips back on flags

--- src/components/Flags.jsx
+++ src/components/Flags.jsx
@@ -1,5 +1,6 @@
 import FlagIcon from '@mui/icons-material/Flag';
 import { FallInG } from './AnimatedG';
+import Tooltip from '@mui/material/Tooltip';
 import Theme from '../constants/Theme';
 
 const styles = {
@@ -23,7 +24,9 @@ function Flags({ flags, k }) {
             return (
             <FallInG key={index} offset={index * 30}>
                 <g transform={`translate(${flag.longitude * 82 - 270 + (k < 0.9 ? 5 * k : 5 * k)},${(55 - flag.latitude) * 137 + 80 + (k < 0.9 ? -5 * k : -10 + 5 * k) })`}>
+                <Tooltip title={flag.name} placement={'top'}>
                     <FlagIcon sx={styles.flag} width={40 / Math.max(1, k)} height={40 / Math.max(1, k)} />
+                </Tooltip>
                 </g>
             </FallInG>
             )
